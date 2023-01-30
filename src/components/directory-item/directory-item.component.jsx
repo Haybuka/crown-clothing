48608e36b46650ae673a88557bf3710d1e15e444
haybuka <paschal.nduleo@gmail.com>
2023-01-30 16:51:59 +0100

    update

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
 } from "./directory-item.styles.jsx";
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const navigate = useNavigate();
+  const { imageUrl, title, route } = category;
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl}></BackgroundImage>
       <Body>
         <h2>{title}</h2>
