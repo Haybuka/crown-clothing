bb32776e5d3293a77500300da51da41d508fb01b
haybuka <paschal.nduleo@gmail.com>
2023-02-02 21:29:55 +0100

    adjusting current user contex for usereducer

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -10,9 +10,34 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in useReducer`);
+  }
+  // return {
+  //    currentUser:payload
+  // }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  // const value = { currentUser, setCurrentUser };
 
   // signOutUser()
   useEffect(() => {
